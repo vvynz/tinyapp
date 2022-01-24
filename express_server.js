11da4b42cbb2c7638c3534b77edce1cbf346c1fe
Vivian Zhang <vvynz12@gmail.com>
2022-01-24 22:47:21 +0000

    new route, shortURL, added. Using the route parameter for shortURL, to look up the associated longURL, both these values are passed to the templateVars object.

--- express_server.js
+++ express_server.js
@@ -21,7 +21,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars); //passes the url data to our template
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
