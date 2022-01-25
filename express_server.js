c540b4a5e4a4a05c8cbfd00a87bcc03da76201c5
Vivian Zhang <vvynz12@gmail.com>
2022-01-25 01:08:57 +0000

    can now handle POST requests. Whenever a submission is received, it generates a random string and saves it to our URLDatabase along with the longURL.

--- express_server.js
+++ express_server.js
@@ -1,53 +1,59 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
-// app.set("view engine", "ejs"); //sets ejs as the view(templating) engine
+app.set("view engine", "ejs"); //sets ejs as the view(templating) engine
 
-// const bodyParser = require("body-parser"); //bodyParser is need to make certain data readable to humans.
-// app.use(bodyParser.urlencoded({extended: true}));
+const bodyParser = require("body-parser"); //bodyParser is need to make certain data readable to humans.
+const { response } = require("express");
+app.use(bodyParser.urlencoded({extended: true}));
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
-// // route added for /urls
-// app.get("/urls", (req, res) => {
-//   const templateVars = { urls: urlDatabase };
-//   res.render("urls_index", templateVars); //passes the url data to our template
-// });
+// route added for /urls
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars); //passes the url data to our template
+});
 
-// app.post("/urls", (req, res) => {
-//   console.log(req.body); //Log the POST request body to the console
-//   res.send("ok");
-// })
+app.post("/urls", (req, res) => {
+  console.log(req.body); //Log the POST request body to the console
+  let randomURL = generateRandomString();
+  urlDatabase[randomURL] = req.body.longURL;
+  console.log(urlDatabase);
 
-// // this route renders the urls_new template in the browser and displays the form to the user
-// app.get("/urls/new", (req, res) => {
-//   res.render("urls_new");
-// });
+  res.send(`${response.statusCode} Submission received! We will now redirect to...`);
+  // res.redirect(`/urls/${randomURL}`);
+})
 
-// app.get("/urls/:shortURL", (req, res) => {
-//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-//   res.render("urls_show", templateVars);
-// });
+// this route renders the urls_new template in the browser and displays the form to the user
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 function generateRandomString() {
   const numCharSet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -58,7 +64,7 @@ function generateRandomString() {
     let output = Math.floor(Math.random() * numCharSet.length);
     randomStr += numCharSet.substring(output, output + 1);
   }
-   console.log(randomStr);
+  //  console.log(randomStr);
   return randomStr;
 };
-console.log(generateRandomString());
+// console.log(generateRandomString());
