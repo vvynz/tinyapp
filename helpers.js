1da6b209c0b96e95fddc246ccb3c888d1a6dd90a
Vivian Zhang <vvynz12@gmail.com>
2022-01-28 21:12:18 +0000

    moved the rest of the helper functios I had in my express_server.js file over to this file and exported them as well.

--- helpers.js
+++ helpers.js
@@ -9,4 +9,28 @@ function getUserByEmail(userEmail, users) {
   return false;
 };
 
-module.exports = { getUserByEmail };
+// a function that returns a user's URLs where the userID is the same to the id of the currently logged in user
+function urlsForUser(ID, urlDatabase) {
+  let userURLs = {};
+  for (let shortURL in urlDatabase) {
+    if(urlDatabase[shortURL].userID === ID) {
+      userURLs[shortURL] = { longURL: urlDatabase[shortURL].longURL };
+    }  
+  }
+  return userURLs;
+};
+
+// a function that generates a random string
+function generateRandomString() {
+  const numCharSet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const length = 6;
+  let randomStr = "";
+
+  for (let i = 0; i < length; i++) {
+    let output = Math.floor(Math.random() * numCharSet.length);
+    randomStr += numCharSet.substring(output, output + 1);
+  }
+  return randomStr;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
