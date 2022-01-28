625753c8485a45abb9f7588d44c81889a8bc7b31
Vivian Zhang <vvynz12@gmail.com>
2022-01-28 22:59:10 +0000

    unit tests written to test the helper function getUserByEmail.

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.isTrue(user.id === expectedUserID);
+  });
+
+  it('should return undefined if an email is not registered in the database', function() {
+    const user = getUserByEmail("nouser@example.com", testUsers)
+    const expectedUserID = undefined;
+    assert.isTrue(user === expectedUserID);
+  });
+});
