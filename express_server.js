11ff9e7cb764c335b3b17dbab090f7908d7838ae
Vivian Zhang <vvynz12@gmail.com>
2022-01-28 19:28:58 +0000

    moved my helper functiion getUserByEmail into a separate file and require the helpers.js file here. Updated all the calls that use the getUserByEmail function. I also refactored the function so that instead of just taking in 1 parameter (email), it now takes in 2 (email, users). Also started to re-oranize and refactor my code.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const cookieSession = require('cookie-session')
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const getUserByEmail = require("./helpers");
 
 app.set("view engine", "ejs"); //sets ejs as the view(templating) engine
 
@@ -38,13 +39,20 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 // route added for /urls
 app.get("/urls", (req, res) => {
-
   if (!req.session.user_id) {
     return res.send("<h2>Sorry you need to be logged in!</h2>");
   }
@@ -63,55 +71,13 @@ app.post("/urls", (req, res) => {
   if (!req.session.user_id) {
     return res.send("Sorry you need to be logged in!");
   };
-  // console.log(req.body); //Log the POST request body to the console
+
   const randomURL = generateRandomString(); //generates a random string as the new random shortURL
   const userID = req.session.user_id;
   urlDatabase[randomURL] = { longURL:req.body.longURL, userID }; //add the new key and value to the URLDatabase
   
   res.redirect(`/urls/${randomURL}`); //redirect to the new page
 });
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  let shortURL = req.params.shortURL;
-  const userID = req.session.user_id;
-
-  const userURLs = urlsForUser(userID);
-
-  // if the shortURL isn't found in the logged in user's own URLs database, they'll be shown an error message
-  if (!Object.keys(userURLs).includes(shortURL)) {
-    return res.send("Sorry you cannot delete these URLs. Please login first!");
-  }
-
-  delete urlDatabase[shortURL]; //should delete the shortURL resource
-  res.redirect("/urls"); //after deleting, redirects back to the index page
-});
-
-// get route will render the update page
-app.get("/urls/:shortURL/update", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL].longURL;
-  const templateVars = { shortURL, longURL, user: users[req.session.user_id] };
-  res.render("urls_show", templateVars);
-});
-
-// post route will take us to the update page and allow the user to update an existing link to a new one
-app.post("/urls/:shortURL/update", (req, res) => {
-  const userID = req.session.user_id;
-
-  const userURLs = urlsForUser(userID);
-
-  if (!Object.keys(userURLs).includes(req.params.shortURL)) {
-    return res.send("Sorry you cannot edit these URLs. Please login to your account first!");
-  }
-
-  let shortURL = req.params.shortURL; //short URL here gets the existing shortURL (of the one that the user wants to update)
-  let newLongURL = req.body.longURL; //getting the input of the new url from the user
-  // urlDatabase = { ...urlDatabase, [shortURL]: newLongURL }; //will update the urlDatabase with the newLongURL, and we can see it updated since the get route has rendered the page above
-  urlDatabase[shortURL].longURL = newLongURL;
-
-  res.redirect("/urls");
-});
-
 
 // this route renders the urls_new template in the browser and displays the form to the user
 app.get("/urls/new", (req, res) => {
@@ -124,22 +90,28 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// tinyURLs page for newly created urls
 app.get("/urls/:shortURL", (req, res) => {
-  if (!req.session.user_id) {
+  const userID = req.session.user_id;
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL].longURL;
+
+  // verifies if the user logged in has an existing account before visiting their urls page
+  if (!userID) {
     return res.send("<h2>Sorry you need to be logged in!</h2>");
   }
 
-  const userURLs = urlsForUser(req.session.user_id);
-  
+  const userURLs = urlsForUser(userID);
+  // if a user is accessing another user's urls page, will be directed to this error message
   if (!Object.keys(userURLs).includes(req.params.shortURL)) {
-    return res.send("<h2>Sorry this short url is not yours!!</h2>");
+    return res.send("<h2>Sorry access denied! Please make sure you're logged into your account!</h2>");
   }
-
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id] };
+  // registered & logged in users will be directed to their short urls page
+  let templateVars = { shortURL: shortURL, longURL: longURL, user: users[userID] };
   res.render("urls_show", templateVars);
 });
 
-
+//clicking the shortURL link will direct users to the website of the long url
 app.get("/u/:shortURL", (req, res) => {
   // if the shortURL id doesn't exist, users will see an error page
   if (!Object.keys(urlDatabase).includes(req.params.shortURL)) {
@@ -155,12 +127,55 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+// GET route will render the short urls page
+app.get("/urls/:shortURL/update", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL].longURL;
+  const templateVars = { shortURL, longURL, user: users[req.session.user_id] };
+  res.render("urls_show", templateVars);
+});
+
+//POST route will take users to the short urls page and allow them to update an existing url
+app.post("/urls/:shortURL/update", (req, res) => {
+  const shortURL = req.params.shortURL; //short URL here gets the existing shortURL (of the one that the user wants to update)
+  const newLongURL = req.body.longURL; //getting the input of the new url from the user
+  urlDatabase[shortURL].longURL = newLongURL; //updated urls will be displayed
+  const userID = req.session.user_id;
+
+  const userURLs = urlsForUser(userID);
+  //For any user who isn't logged in and tries to update another user's short urls page, they'll see this error message
+  if (!Object.keys(userURLs).includes(shortURL)) {
+    return res.send("Sorry permission denied. Please login to your account first!");
+  }
+  // after updating their urls, the user will be redirected back to the /urls (index) page
+  res.redirect("/urls");
+});
+
+// Delete buttons on the /urls page will allow users to delete an url from their database
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const userID = req.session.user_id;
+
+  const userURLs = urlsForUser(userID);
+
+  // if the shortURL isn't found in the logged in user's own URLs database, they'll be shown an error message
+  if (!Object.keys(userURLs).includes(shortURL)) {
+    return res.send("Sorry permission denied. Please login first!");
+  }
+
+  delete urlDatabase[shortURL]; //should delete the shortURL resource
+  res.redirect("/urls"); //after deleting, redirects back to the index page
+});
+
 // GET login route. Renders a login page to the user
 app.get("/login", (req, res) => {
-  // if a user isn't logged in, they'll be redirected back to /urls
-  if (req.session.user_id) {
+  const userID = req.session.user_id;
+
+  // if a registered user is logged in, they'll be redirected back to /urls
+  if (userID) {
     res.redirect("/urls");
   } else {
+    //Users who aren't logged in will be directed to the login page
     const templateVars = { user: null };
     res.render("urls_login", templateVars);
   }
@@ -172,7 +187,7 @@ app.post("/login", (req, res) => {
   const password = req.body.password;
 
   // if user with an email that can't be found, return a 403 status code
-  const user = isUserEmailTaken(email);
+  const user = getUserByEmail(email, users);
 
   if (user === false) {
     return res.status(403).send("403 Error: Sorry a user with that email doesn't exist!");
@@ -190,48 +205,43 @@ app.post("/login", (req, res) => {
 
 // POST route (logout) clears the cookie variable and redirects back to the homepage
 app.post("/logout", (req, res) => {
-  req.session["user_id"] = null;
+  req.session = null;
   res.redirect("/urls");
 });
 
 // GET route takes users to the registration page 
 app.get("/register", (req, res) => {
-  // if a there's a user that hasn't been registered, they'll be directed to the registration page
+  // registered users will be redirected to the /urls (index) page
   if (req.session.user_id) {
     res.redirect("/urls");
   } else {
+    // if a there's a user that hasn't been registered, they'll be directed to the registration page
     const templateVars = { user:null };
     res.render("urls_registration", templateVars);
   }
 });
 
 app.post("/register", (req, res) => {
-  let newUserID = generateRandomString();
+  // an new ID will be generated once a user registers
+  const newUserID = generateRandomString();
   const email = req.body.email;
-  let password = req.body.password;
+  const password = req.body.password;
  
-  if (!email || !password) { //if no email or password are entered
+  if (!email || !password) { //if no email or password are entered, then an error message will be returned
     res.status(400).send("Please enter in an email and a password!");
   };
 
-  // if isUserEmailTaken is true redirect to error page
-  if (isUserEmailTaken(email)) {
+  // if a user registers with an existing email, redirect to error page
+  if (getUserByEmail(email, users)) {
     return res.status(400).send("That email is already registered!");
   } else {
-    // if isUserEmailTaken is false, save the email and new user into our users obj, save the user_id as a cookie and redirect back to /urls
+    //newly registered user's email, password and user ID will be saved into our users obj
     users[newUserID] = { id: newUserID, email, password: bcrypt.hashSync(password, 10) }; //hashing the password
   };
   
+  // save the user_id as a cookie and redirect back to /urls
   req.session.user_id = newUserID;
   res.redirect("/urls");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 // a function that returns a user's URLs where the userID is the same to the id of the currently logged in user
@@ -245,16 +255,6 @@ function urlsForUser(ID) {
   return userURLs;
 };
 
-// a function that checks whether an email exists in our database
-function isUserEmailTaken(userEmail) {
-  let userIDS = Object.keys(users);
-  for (let userID of userIDS) {
-    if (users[userID].email === userEmail) {
-      return users[userID];
-    }
-  }
-  return false;
-};
 
 // a function that generates a random string
 function generateRandomString() {
